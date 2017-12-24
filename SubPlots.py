88f9158e8cd7496e9f82442b752d647611559afa
Manthan Thakker <manthan.thakker@pingup.com>
2017-12-24 14:10:13 +0530

    Update SubPlots.py

--- SubPlots.py
+++ SubPlots.py
@@ -11,15 +11,23 @@ x=x+.2
 
 
 x=x+.2
-plt.subplot(221)
+plt.subplot(221,facecolor='lightblue')
+plt.xlabel('x label')
+plt.ylabel('y label')
 plt.plot(x,y,'-')
-plt.subplot(222)
+plt.subplot(222,facecolor='lightblue')
+plt.xlabel('x label')
+plt.ylabel('y label')
 x=x+.2
 plt.plot(x,y,'X')
-plt.subplot(223)
+plt.subplot(223,facecolor='lightblue')
+plt.xlabel('x label')
+plt.ylabel('y label')
 x=x+.2
 plt.plot(x,y,'-')
-plt.subplot(224)
+plt.subplot(224,facecolor='lightblue')
+plt.xlabel('x label')
+plt.ylabel('y label')
 x=x+.2
 plt.plot(x,y,'X')
 plt.title('Sample 2-d Graph')
