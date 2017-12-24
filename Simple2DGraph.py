a01ff33776bc3344f90c82ec5e456a2934bd61c3
Manthan Thakker <manthan.thakker@pingup.com>
2017-12-24 13:29:25 +0530

    Update Simple2DGraph.py

--- Simple2DGraph.py
+++ Simple2DGraph.py
@@ -11,3 +11,8 @@ plt.title('Sample 2-d Graph')
 plt.ylabel('Y axis  label')
 plt.xlabel('X axis label')
 plt.show()
+
+
+
+
+## Refer https://matplotlib.org/gallery/pyplots/pyplot_simple.html#sphx-glr-gallery-pyplots-pyplot-simple-py
