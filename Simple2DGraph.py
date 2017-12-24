35672d8b85cff4951b96825a635e785936f29095
Manthan Thakker <manthan.thakker@pingup.com>
2017-12-24 13:28:40 +0530

    Create Simple2DGraph.py

---
+++ Simple2DGraph.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.random.rand(10)
+y=np.random.rand(10)
+x.sort()
+y.sort()
+
+plt.plot(x,y)
+plt.title('Sample 2-d Graph')
+plt.ylabel('Y axis  label')
+plt.xlabel('X axis label')
+plt.show()
